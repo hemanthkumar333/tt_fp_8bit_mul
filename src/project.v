7d9393746e6c7c56b833c4c85dd49bc737cf6eda
hemanthkumar333 <47243724+hemanthkumar333@users.noreply.github.com>
2025-01-22 00:08:24 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -22,66 +22,71 @@ module tt_um_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{uio_in,ena, clk, rst_n, 1'b0};
-      daddamul uut (
-          .A(ui_in[3:0]),
-          .B(ui_in[7:4]),
-          .Y(uo_out[7:0])
+    wire _unused = &{ena, clk, rst_n, 1'b0};
+      fp_mul_8bit uut (
+          .flp_a(ui_in[7:0]),
+          .flp_b(uio_in[7:0]),
+          .result(uo_out[7:0])
     );
 endmodule
 
-module daddamul(A,B,Y);
-input [3:0] A;
-input [3:0] B;   
-output [7:0] Y;
+module fp_mul_8bit(flp_a,flp_b,result);
+input [7:0] flp_a;
+input [7:0] flp_b;   
+output [7:0] result;
 
-wire sum[3:0];
-wire hcar[3:0];
-wire smm[2:0];
-wire caar[3:0];
-wire crr[2:0];
-// generating products.
-wire  gen_pp [0:3][3:0];    
+reg sign,
+reg [2:0] exponent;
+reg [3:0] prod,
+reg [7:0] result;
+reg [2:0] exp_a, exp_b;
+reg [3:0] exp_sum; 
+reg [3:0] fract_a, fract_b;
+reg [7:0] prod_dbl;
+reg [2:0] exp_unbiased;
+  
 genvar i;
-genvar j;
 
- for(i = 0; i<4; i=i+1)begin
-   for(j = 0; j<4;j = j+1)begin
-      assign gen_pp[i][j] = A[j]*B[i];
-   end
- end
+always @ (flp_a or flp_b)
+    begin
+		sign = flp_a[7] ^ flp_b[7];
+        exp_a = flp_a[6:4];
+        exp_b = flp_b[6:4];
+        fract_a = {1'b1, flp_a[3:1]}; // Implicit leading 1
+        fract_b = {1'b1, flp_b[3:1]}; 
+	 
+        exp_sum = exp_a + exp_b;
+
+        // Remove bias
+        exp_unbiased = exp_sum - 3'b011;
+
+        prod_dbl = fract_a * fract_b;
+        prod = prod_dbl[5:2]; 
+		  
+        if (prod == 0) begin
+            result = 8'b0; 
+        end else begin
+            for (i = 0; i < 4; i = i + 1) begin
+						if (prod[3] == 0 && exp_unbiased > 0) begin
+							 prod = prod << 1;
+							 exp_unbiased = exp_unbiased - 1;
+						end
+            end 
+
+            exponent = exp_unbiased;
+			if (flp_a[6:0] == 0 || flp_b[6:0] == 0) begin
+				result = 8'b0; 
+			end
+			else if (exp_unbiased > 3'b111) begin
+				 result = {sign, 3'b111, 4'b0}; // Overflow to infinity
+			end 
+			else if (exp_unbiased < 3'b000) begin
+				 result = 8'b0; // Underflow to zero
+			end 
+			else begin
+				 result = {sign, exp_unbiased, prod[3:0]}; // Normal case
+			end
+        end
+    end
  
- //LEVEL 1: 4 to 3 reduction
- assign Y[0]= gen_pp[0][0]; //m1
- fulladd fle0(.p1(gen_pp[1][0]),.q1(gen_pp[0][1]),.car(0),.fad(Y[1]), .cout(hcar[0]) ) ; 
- fulladd fle1(.p1(gen_pp[2][0]), .q1(gen_pp[1][1]), .car(gen_pp[0][2]),.fad(sum[1]),.cout(hcar[1]));
- fulladd fle2(.p1(gen_pp[3][0]), .q1(gen_pp[2][1]), .car(gen_pp[1][2]),.fad(sum[2]),.cout(hcar[2]));
- fulladd fle3(.p1(gen_pp[3][1]), .q1(0), .car(gen_pp[2][2]),.fad(sum[3]),.cout(hcar[3]));
- 
- //LEVEL 1: 3 to 2 reduction
- fulladd fg0(.p1(hcar[0]),.q1(sum[1]),.car(0),.fad(Y[2]), .cout(caar[0]) ) ; 
- fulladd fg1(.p1(gen_pp[0][3]), .q1(sum[2]), .car(hcar[1]),.fad(smm[0]),.cout(caar[1]));
- fulladd fg2(.p1(sum[3]),.q1(hcar[2]),.car(gen_pp[1][3]),.fad(smm[1]), .cout(caar[2]) );
- fulladd fg3(.p1(gen_pp[3][2]), .q1(hcar[3]), .car(gen_pp[2][3]),.fad(smm[2]),.cout(caar[3]));
- 
- //Level 3: 2 to 1 
- fulladd ff0(.p1(smm[0]), .q1(caar[0]), .car(0),.fad(Y[3]),.cout(crr[0]));
- fulladd ff1(.p1(smm[1]), .q1(caar[1]), .car(crr[0]),.fad(Y[4]),.cout(crr[1]));
- fulladd ff2(.p1(smm[2]), .q1(caar[2]), .car(crr[1]),.fad(Y[5]),.cout(crr[2]));
- fulladd ff3(.p1(gen_pp[3][3]), .q1(caar[3]), .car(crr[2]),.fad(Y[6]),.cout(Y[7]));
- 
-endmodule
-
-module fulladd(q1,p1,fad,car,cout);
-input q1,p1,car;
-output fad,cout;
-
-wire w1= q1^p1;
-xor (fad,w1,car);
-
-wire a1= w1&car;
-wire a2= q1&p1;
-
-or(cout,a1,a2);
-
 endmodule
